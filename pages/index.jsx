e7fa3bc35292e3e23a070637fe18565829f88196
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-18 10:40:27 -0600

    Setting up file structures and views.

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import { Container }from '@material-ui/core'
+
+const Home = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Brian Blankenship</title>
+        <meta name="description" content="" />
+      </Head>
+    </Container>
+  )
+};
+
+export default Home;
