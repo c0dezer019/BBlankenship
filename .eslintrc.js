25715aca0b861eeb82c918ddd3034c9b21514e43
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-19 21:34:10 -0600

    Reinstalled packages.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,48 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'airbnb',
-        'plugin:prettier/recommended',
-        'prettier/flowtype',
-        'prettier/react',
-        'prettier/babel',
-        'prettier',
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'prettier/prettier': 0,
+    'block-spacing': 2,
+    camelcase: [
+      'error', { properties: 'never' },
     ],
-    parserOptions: {
-        ecmaFeatures: { jsx: true },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    parser: 'babel-eslint',
-    plugins: ['react', 'prettier', 'babel'],
-    rules: {
-        'prettier/prettier': 0,
-        'block-spacing': 2,
-        camelcase: [
-            'error', { properties: 'never' },
-        ],
-        'jsx-quotes': ['error', 'prefer-double'],
-        'arrow-parens': [
-            0,
-            'as-needed',
-            { requireForBlockBody: true },
-        ],
-        'react/jsx-pascal-case': 2,
-        'prefer-const': 0,
-        'no-console': 0,
-        'react/self-closing-comp': 0,
-        'react/jsx-props-no-spreading': 0,
-        'import/prefer-default-export': 0,
-        'react/forbid-prop-types': 0,
-        'no-nested-ternary': 0,
-        'react/prop-types': 0,
-        'array-element-newline': ["error", {
-            "ArrayExpression": "consistent",
-            "ArrayPattern": { "minItems": 20 }
-        }],
-        'no-plusplus': 0
-    },
+    'jsx-quotes': ['error', 'prefer-double'],
+    'arrow-parens': [
+      0,
+      'as-needed',
+      { requireForBlockBody: true },
+    ],
+    'react/jsx-pascal-case': 2,
+    'prefer-const': 0,
+    'no-console': 0,
+    'react/self-closing-comp': 0,
+    'react/jsx-props-no-spreading': 0,
+    'import/prefer-default-export': 0,
+    'react/forbid-prop-types': 0,
+    'no-nested-ternary': 0,
+    'react/prop-types': 0,
+    'array-element-newline': ['error', {
+      ArrayExpression: 'consistent',
+      ArrayPattern: { minItems: 20 },
+    }],
+    'no-plusplus': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+  },
 };
