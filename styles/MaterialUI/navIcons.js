f88853a9bd5e0623cbba47271376978f70786d7d
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-22 20:33:10 -0600

    Adjusting margin of icons.

--- styles/MaterialUI/navIcons.js
+++ styles/MaterialUI/navIcons.js
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(({
   icon: {
-    marginRight: '-6px',
+    marginRight: '-2em',
     width: 100,
     height: '1em',
     position: 'relative',
