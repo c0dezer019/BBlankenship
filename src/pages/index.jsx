a8ad7e8c1f95b78165e996a8b8fe5ca3246bd6f4
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-22 20:45:47 -0600

    Adding stylesheet for homepage.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import { Container } from '@material-ui/core';
+import '../../styles/sass/home.css';
 
 const Home = () => (
   <Container maxWidth="xl">
