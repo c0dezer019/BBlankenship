233f63672fc60908d4c09c33fcfc02638ba25c7e
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-28 14:46:25 -0600

    Added text elements.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import Head from 'next/head';
-import { Box, Grid, Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
+import styledTypo from '../../styles/MaterialUI/styledTypo';
 
-const Home = () => (
-  <Box className="body" maxWidth="xl" height="100%">
-    <Head>
-      <title>Brian Blankenship</title>
-    </Head>
-    <p>Welcome, user.</p>
-    <p>
-      I am
-      <span> Brian Blankenship</span>
-    </p>
-    <Typography variant="h2">Software Engineer.</Typography>
-    <Typography variant="h3">Thinker.</Typography>
-    <Typography variant="h4">Problem solver.</Typography>
-    <Typography variant="h5">Teacher.</Typography>
-  </Box>
-);
+const Home = () => {
+  const classes = styledTypo();
+
+  return (
+    <Box className="body" maxWidth="xl" height="100%">
+      <Head>
+        <title>Brian Blankenship</title>
+      </Head>
+      <p>Welcome, user.</p>
+      <p>
+        I am
+        <span> Brian Blankenship</span>
+        , a
+      </p>
+      <div className="headings" id="1">
+        <Typography className={classes.root} variant="h3">Software Engineer,</Typography>
+      </div>
+      <div className="headings" id="2">
+        <Typography className={classes.root} variant="h4">Thinker,</Typography>
+      </div>
+      <div className="headings" id="3">
+        <Typography className={classes.root} variant="h5">Problem solver,</Typography>
+      </div>
+      <p>and</p>
+      <div className="headings" id="4">
+        <Typography className={classes.root} variant="h6">Teacher.</Typography>
+      </div>
+    </Box>
+  );
+};
 
 export default Home;
