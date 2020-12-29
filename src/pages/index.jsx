57999b3f89561fc2038ce4510c00f59b9cdc8145
Brian Blankenship <brian.ga.edu@gmail.com>
2020-12-28 19:03:39 -0600

    Gridifying everything

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,35 +1,59 @@
 import React from 'react';
 import Head from 'next/head';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Grid, Typography } from '@material-ui/core';
 import styledTypo from '../../styles/MaterialUI/styledTypo';
+import styles from '../../styles/sass/home.module.css';
 
 const Home = () => {
   const classes = styledTypo();
 
   return (
-    <Box className="body" maxWidth="xl" height="100%">
+    <Box className={styles.body} maxWidth="xl">
       <Head>
         <title>Brian Blankenship</title>
       </Head>
-      <p>Welcome, user.</p>
-      <p>
-        I am
-        <span> Brian Blankenship</span>
-        , a
-      </p>
-      <div className="headings" id="1">
-        <Typography className={classes.root} variant="h3">Software Engineer,</Typography>
-      </div>
-      <div className="headings" id="2">
-        <Typography className={classes.root} variant="h4">Thinker,</Typography>
-      </div>
-      <div className="headings" id="3">
-        <Typography className={classes.root} variant="h5">Problem solver,</Typography>
-      </div>
-      <p>and</p>
-      <div className="headings" id="4">
-        <Typography className={classes.root} variant="h6">Teacher.</Typography>
-      </div>
+      <Grid container id="text-container">
+        <Grid item xl={1}>
+          &nbsp;
+        </Grid>
+        <Grid item xl={11}>
+          <p className={styles.intro}>
+            I am
+            <span> Brian Blankenship</span>
+            , a
+          </p>
+        </Grid>
+        <Grid item xl={2}>
+          &nbsp;
+        </Grid>
+        <Grid item xl={10} id="heading-1">
+          <Typography className={classes.root} id="test" variant="h3">Software Engineer,</Typography>
+        </Grid>
+        <Grid item xl={5}>
+          &nbsp;
+        </Grid>
+        <Grid item xl={7} id="heading-2">
+          <Typography className={classes.root} variant="h4">Thinker,</Typography>
+        </Grid>
+        <Grid item xl={6}>
+          &nbsp;
+        </Grid>
+        <Grid item xl={6} id="heading-3">
+          <Typography className={classes.root} variant="h5">Problem solver,</Typography>
+        </Grid>
+        <Grid item xl={7}>
+          &nbsp;
+        </Grid>
+        <Grid item xl={5}>
+          <p className={styles.and}>and</p>
+        </Grid>
+        <Grid item xl={8}>
+          &nbsp;
+        </Grid>
+        <Grid item xl={4} id="heading-4">
+          <Typography className={classes.root} variant="h6">Professional Coffee Drinker.</Typography>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
---
+++ styles/MaterialUI/styledContainer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container, withStyles } from '@material-ui/core';
+
+const styledContainer = withStyles({
+  root: {
+    height: '100%',
+    position: 'relative',
+    margin: 'auto',
+    marginTop: '50px',
+    width: '100%',
+  },
+})(({ classes, children }) => (
+  <Container className={classes.root} maxWidth="xl">
+    {children}
+  </Container>
+));
+
+export default styledContainer;
