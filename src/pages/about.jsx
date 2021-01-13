3f69376551d1c56f4dae741b12605c7985b9e829
Brian Blankenship <brian.ga.edu@gmail.com>
2021-01-12 20:09:08 -0600

    Adding styling for about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
 import Head from 'next/head';
+import { Box, Typography } from '@material-ui/core';
+import StyledContainer from '../styles/MaterialUI/StyledContainer';
+import styledTypo from '../styles/MaterialUI/styledTypo';
+import styles from '../styles/sass/about.module.css';
 
-const About = () => (
-  <Box className="body" maxWidth="xl">
-    <Head>
-      <title>About</title>
-      <meta name="description" content="About Brian Blankenship" />
-    </Head>
-    <div>
-      <h1>Hello</h1>
-    </div>
-  </Box>
-);
+const About = () => {
+  const classes = styledTypo();
+
+  return (
+    <Box className="contentContainer" maxWidth="xl">
+      <Head>
+        <title>About</title>
+        <meta name="description" content="About Brian Blankenship" />
+      </Head>
+      <StyledContainer>
+        <article className={styles.missionStatement}>
+          <Typography className={classes.root} variant="h4">#Mission</Typography>
+          <p>
+            I am a Full Stack Software Engineer motivated to grow with a team and dedicated to learning the latest
+            technologies.Whether it’s building a robust, functional form or updating data without delaying user
+            experience, I aim to solve these problems with my experience in leading full stack web technologies such as
+            Javascript, React, HTML5, Node,js, and Python.
+          </p>
+        </article>
+      </StyledContainer>
+    </Box>
+  );
+};
 
 export default About;
