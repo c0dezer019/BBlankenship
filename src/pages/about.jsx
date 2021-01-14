5ecc7b769816dab271d8a2b19c058f1c9ebe3226
Brian Blankenship <brian.ga.edu@gmail.com>
2021-01-13 23:53:13 -0600

    Sectioning off content for tech stack.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
-import { Box, Typography } from '@material-ui/core';
+import {
+  Box, Container, GridList, GridListTile, Typography,
+} from '@material-ui/core';
 import StyledContainer from '../styles/MaterialUI/StyledContainer';
 import styledTypo from '../styles/MaterialUI/styledTypo';
 import styles from '../styles/sass/about.module.css';
@@ -15,15 +17,22 @@ const About = () => {
         <meta name="description" content="About Brian Blankenship" />
       </Head>
       <StyledContainer>
-        <article className={styles.missionStatement}>
-          <Typography className={classes.root} variant="h4">#Mission</Typography>
-          <p>
-            I am a Full Stack Software Engineer motivated to grow with a team and dedicated to learning the latest
-            technologies.Whether it’s building a robust, functional form or updating data without delaying user
-            experience, I aim to solve these problems with my experience in leading full stack web technologies such as
-            Javascript, React, HTML5, Node,js, and Python.
-          </p>
-        </article>
+        <Container maxWidth="xl" id="missionStatementContainer">
+          <article className={styles.missionStatement}>
+            <Typography className={classes.root} id="missionStatementText" variant="h4">#mission</Typography>
+            <p>
+              I am a Software Engineer with experience in Full Stack Development and cloud deployment. Whether it’s
+              building a robust, functional form or updating data without delaying user experience, I aim to solve
+              these problems with my experience in leading technologies such as Javascript, React, HTML5, Node.js, and
+              Python.
+            </p>
+          </article>
+        </Container>
+        <Container maxWidth="xl" id="technologiesContainer">
+          <section id="technologies">
+            <Typography className={classes.root}>#technologies</Typography>
+          </section>
+        </Container>
       </StyledContainer>
     </Box>
   );
