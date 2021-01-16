06bd3bf69211126e22167e159ec6cd3dbe92e911
Brian Blankenship <brian.ga.edu@gmail.com>
2021-01-16 01:33:35 -0600

    Customized grid.

---
+++ src/styles/MaterialUI/styledGrid.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    margin: 0,
+    marginBottom: '1rem',
+    padding: 0,
+  },
+  cell: {
+    flexBasis: 0,
+  },
+});
+
+export default useStyles;
