db44a95be962f36f46e4f5a721ab531b07a165ae
Brian Blankenship <brian.ga.edu@gmail.com>
2021-01-18 15:39:09 -0600

    Semanticizing component.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import { Box } from '@material-ui/core';
 import StyledContainer from '../styles/MaterialUI/StyledContainer';
+import styles from '../styles/sass/project.module.css';
 
 const ProjectCard = ({ project }) => {
-  const { name, description, screenshot, tech, url } = project;
+  const { description, descStyleRule, name, screenshot, screenshotStyleRule, tag, tech, url } = project;
+  const projectDesc = classNames('projectDesc', descStyleRule);
+  const projectImage = classNames('projectImage', styles.imageContainer);
 
   return (
-    <StyledContainer maxWidth="xl" flex row>
-      <Box className="projectCard" id={`${name}_image`}>
-        <img src={screenshot} alt={name} />
-      </Box>
+    <StyledContainer className="projectCard" display="flex" id={ `${tag}_card` } maxWidth="xl">
+      <section className={ styles.dataContainer }>
+        <Box className={ projectImage } id={ `${tag}_image` }>
+          <img className={ screenshotStyleRule } src={ screenshot } alt={ name } />
+        </Box>
+        <Box className="projectInfo" id={ `${tag}_info` }>
+          <Box classname={ projectDesc } id={ `${tag}_desc` }>
+            <article id="descriptionText">
+              <p>
+                { description }
+              </p>
+            </article>
+          </Box>
+        </Box>
+      </section>
     </StyledContainer>
   );
 };
@@ -20,6 +35,7 @@ ProjectCard.propTypes = {
     name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     screenshot: PropTypes.string.isRequired,
+    styleRules: PropTypes.arrayOf(PropTypes.string),
     tech: PropTypes.arrayOf(PropTypes.string).isRequired,
     url: PropTypes.string.isRequired,
   }).isRequired,
