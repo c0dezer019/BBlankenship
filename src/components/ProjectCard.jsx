9ed7a51ac7907e67afa2215a170a8105950e1da6
Brian Blankenship <brian.ga.edu@gmail.com>
2021-01-18 23:56:10 -0600

    Project card draft 1.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Box } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
+import StyledChip from '../styles/MaterialUI/StyledChip';
 import StyledContainer from '../styles/MaterialUI/StyledContainer';
+import styledTypo from '../styles/MaterialUI/styledTypo';
 import styles from '../styles/sass/project.module.css';
 
 const ProjectCard = ({ project }) => {
   const { description, name, screenshot, tag, tech, url } = project;
   const projectTextArea = classNames('projectInfo', styles.projectTextArea);
   const projectImage = classNames('projectImage', styles.imageContainer);
+  const classes = styledTypo();
 
   return (
     <StyledContainer className="projectCard" display="flex" id={ `${tag}_card` } maxWidth="xl">
@@ -17,12 +20,25 @@ const ProjectCard = ({ project }) => {
           <img className={ styles.screenshotImage } src={ screenshot } alt={ name } />
         </Box>
         <Box className={projectTextArea} id={ `${tag}_info` }>
-          <Box classname="projectDesc" id={ `${tag}_desc` }>
+          <Box className="projectDesc" id={ `${tag}_desc` }>
             <article id="descriptionText">
+              <header>
+                <Typography className={classes.root} variant="h6">#{name}</Typography>
+              </header>
               <p>
                 { description }
               </p>
             </article>
+          </Box>
+          <Box className="projectStack" id={`${tag}_stack`}>
+            <section>
+              <header>
+                <Typography className={classes.root} variant="h6">tech_stack</Typography>
+              </header>
+              { tech.map((tech, i) => (
+                <StyledChip key={`${i + 1}_chip`} label={ tech } variant="outlined" />
+              )) }
+            </section>
           </Box>
         </Box>
       </section>
