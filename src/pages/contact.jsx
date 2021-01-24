3d2fc4edf18a377d869f5814020523f9990f862a
Brian Blankenship <brian.ga.edu@gmail.com>
2021-01-24 12:55:24 -0600

    Adding contact form.

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Head from 'next/head';
 import { Box, Typography } from '@material-ui/core';
 import data from '../data/projectData';
+import ContactForm from '../components/ContactForm';
 import GridTiles from '../components/GridTiles';
-import StyledContainer from '../styles/material_ui/StyledContainer';
 import styledTypo from '../styles/material_ui/styledTypo';
 import muiStyles from '../styles/material_ui/muiStyles';
 import styles from '../styles/sass/contact.module.css';
@@ -24,23 +24,19 @@ const Contact = () => {
   ];
 
   return (
-    <Box className={ classes.root } maxWidth="xl">
+    <Box className={ classes.contact } maxWidth="xl">
       <Head>
         <title>Contact Me</title>
       </Head>
-      <StyledContainer id="contentContainer" flex col>
-        <header>
-          <Typography className={ typo.root } variant="h5">#contact_me</Typography>
-        </header>
-        <section className={ styles.contact } id="contact">
-          <p>
-            Hello World
-          </p>
-        </section>
-        <footer className={ styles.footer }>
-          <GridTiles data={ contactMethods } />
-        </footer>
-      </StyledContainer>
+      <header>
+        <Typography className={ typo.contactHeader } variant="h5">#contact_me</Typography>
+      </header>
+      <section className={ styles.contact } id="contact">
+        <ContactForm />
+      </section>
+      <footer className={ styles.footer }>
+        <GridTiles data={ contactMethods } />
+      </footer>
     </Box>
   );
 };
