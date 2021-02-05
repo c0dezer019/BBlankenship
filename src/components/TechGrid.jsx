cc93302143f0e66d451df573d02a9153d902e4f6
Brian Blankenship <brian.ga.edu@gmail.com>
2021-02-04 19:17:32 -0600

    Displays images instead of chips.

--- src/components/TechGrid.jsx
+++ src/components/TechGrid.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Container, Grid, Typography } from '@material-ui/core';
-import StyledChip from '../styles/material_ui/StyledChip';
 import muiStyles from '../styles/material_ui/muiStyles';
 
 const TechGrid = ({ category, tech }) => {
   const classes = muiStyles();
 
   return (
-    <Container className={classes.techGrid}>
+    <Container className={ classes.techGrid }>
       <Typography variant="h6">
         { category }
       </Typography>
-      <Grid container className="techStack" id={ category } spacing={2}>
+      <Grid container className="techStack" id={ category } spacing={ 2 }>
         { tech.map((name, i) => (
           <Grid className={ classes.techGridCell } item key={ i + 1 } xl={ 2 }>
-            <StyledChip label={ name } variant="outlined" />
+            { name === 'bootstrap.svg' || name === 'python.svg' ?
+              (<img src={ `../../images/tech_icons/${ name }` } alt={ name } width="100px" />) :
+              (<img src={ `../../images/tech_icons/${ name }` } alt={ name } width="70px" />) }
           </Grid>
         )) }
       </Grid>
