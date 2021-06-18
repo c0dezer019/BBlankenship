ce326ea2dc7b938b5cd38293994f562348a9de36
c0dezer019 <c0dezer019@bblanke nship.me>
2021-06-18 01:19:26 -0500

    Added the navbar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,70 @@
-import React from 'react';
-import {
-  ContactSupportOutlined,
-  FolderSpecialOutlined,
-  HomeOutlined,
-  InfoOutlined,
-} from '@material-ui/icons';
+import styled from 'styled-components';
+import ThreeLineMenu from './subcomponents/ThreeLineMenu';
+
+const Container = styled.div`
+  height: 4.5em;
+  width: 100%;
+  padding: 21px;
+  
+  @media screen and (max-width: 1279px) {
+    display: none;
+    height: 100%;
+    width: 40vw;
+  }
+  
+  @media screen and (max-width: 600px) {
+    width: 50vw;
+  }
+  
+  @media screen and (max-width: 500px) {
+    width: 100vw;
+  }
+  
+`;
+
+const LinkContainer = styled.div`
+  height: 100%;
+  margin: auto 0 auto 6%;
+  width: 30%;
+
+  @media screen and (min-width: 1280px) {
+    width: 100%;
+  }
+  
+  @media screen and (max-width: 1279px) {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const NavLink = styled.a`
+  font-size: 1.5rem;
+  margin: auto 10px;
+  text-decoration: none;
+
+  &:hover {
+    color: #9999d2;
+    text-decoration: underline;
+    }
+  
+  @media screen and (max-width: 1279px) {
+    display: block;
+  }
+`;
 
 const NavBar = () => (
-  <div className="container">
-    <style jsx>{`
-      .container {
-        border: 1px solid white;
-        width: 100%;
-      }
-    `}
-    </style>
-
-  </div>
+  <>
+    <ThreeLineMenu />
+    <Container>
+      <LinkContainer id="link-container">
+        <NavLink className="nav-link" href="#home">Home</NavLink>
+        <NavLink className="nav-link" href="#about">About</NavLink>
+        <NavLink className="nav-link" href="#techStack">The Stack</NavLink>
+        <NavLink className="nav-link" href="#projects">Projects</NavLink>
+        <NavLink className="nav-link" href="#contact">Contact</NavLink>
+      </LinkContainer>
+    </Container>
+  </>
 );
 
 export default NavBar;
--- src/components/subcomponents/ThreeLineMenu.tsx
+++ src/components/subcomponents/ThreeLineMenu.tsx
@@ -1,23 +1,65 @@
-import { styled } from 'styled-components';
+// import { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 
-const NavContainer = styled.div`
-  width: 30px;
-  display: block;
-  margin: 1em 0 0 1em;
+const rotateContainer = keyframes` 
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(90deg);
+  }
 `;
 
-const Bar = styled.div`
+const scaleBar = keyframes`
+  from {
+    transform: scaleX(1);
+  }
+  
+  to {
+    transform: scaleX(3);
+  }
+`;
+
+const NavContainer = styled.div`
+  display: block;
+  margin: 1em 0 0 1em;
+  padding: 0;
+  width: 30px;
+  
+  @media screen and (min-width: 1279px) {
+    display: none;
+  }
+`;
+
+const LeftBar = styled.div`
   background-color: white;
   border-radius: 25px;
   height: 4px;
-  margin: 5px 0;
+  margin: 6px auto;
+  width: 15px;
+`;
+
+const MiddleBar = styled.div`
+  background-color: white;
+  border-radius: 25px;
+  height: 4px;
+  margin: 6px 0;
+  // width: 20px;
+`;
+
+const RightBar = styled.div`
+  background-color: white;
+  border-radius: 25px;
+  height: 4px;
+  margin: 6px auto;
+  width: 15px;
 `;
 
 const ThreeLineMenu = () => (
-  <NavContainer>
-    <Bar />
-    <Bar />
-    <Bar />
+  <NavContainer id="nav-container">
+    <LeftBar className="menu-bar" />
+    <MiddleBar className="menu-bar" />
+    <RightBar className="menu-bar" />
   </NavContainer>
 );
 
