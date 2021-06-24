b610bba7ab8a06b6abc3166dcff934d2cb51906a
c0dezer019 <c0dezer019@bblanke nship.me>
2021-06-24 16:05:26 -0500

    Intro stuff.

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,20 +1,10 @@
 import { FC, ReactElement } from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  color: white;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-`;
+import IntroContainer from './styled/IntroContainer';
 
 const Intro: FC = (): ReactElement => (
-  <Container id="intro-parent-container">
+  <IntroContainer id="intro-parent-container">
     <p>Test</p>
-  </Container>
+  </IntroContainer>
 );
 
 export default Intro;
---
+++ src/components/styled/IntroContainer.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const IntroContainer = styled.div`
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export default IntroContainer;
