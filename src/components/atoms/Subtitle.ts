ca9af21587c9cd1e562e65dfba166b49ef4a61a8
Brian Blankenship <c0dezer019@zohomail.com>
2022-05-12 18:36:33 -0500

    Header and subtitle elements.

---
+++ src/components/atoms/Subtitle.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Subtitle = styled.p`
+  font-size: 1.7rem;
+  margin: 0;
+
+  @media screen and (max-width: 800px) {
+    font-size: 0.8rem;
+  }
+`;
+
+export default Subtitle;
